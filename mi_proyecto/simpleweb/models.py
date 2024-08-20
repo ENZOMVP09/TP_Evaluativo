38c298756bbb2cead2b5f26fffcf1663afcf391d
ENZOMVP09 <hugosuica1905@gmail.com>
2024-08-19 21:10:07 -0300

    commit

---
+++ mi_proyecto/simpleweb/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class TuModelo(models.Model):
+    campo1 = models.CharField(max_length=100)
+    campo2 = models.IntegerField()
+    is_deleted = models.BooleanField(default=False)
+
+# Create your models here.
